24c1f154141d3d3cca02368b12fc67bc42d5f7c5
Opilane TTHK <opilane@tthk.ee>
2023-10-23 16:24:08 +0300

    init

--- Shop/Shop.SpaceshipTest/SpaceshipTest.cs
+++ Shop/Shop.SpaceshipTest/SpaceshipTest.cs
@@ -1,5 +1,6 @@
 ﻿using ShopCore.Dto;
 using ShopCore.ServiceInterface;
+using System;
 
 
 namespace Shop.SpaceshipTest
@@ -31,6 +32,74 @@ namespace Shop.SpaceshipTest
             Guid guid = Guid.Parse("204d3f40-0d9c-4adf-8bd2-602823455f47");
             //kuidas teha automaatselt guidi??
             Guid wrongGuid = Guid.Parse(Guid.NewGuid().ToString());
+
+            await Svc<ISpaceshipServices>().GetAsync(guid);
+
+
+            Assert.NotEqual(guid, wrongGuid);
+        }
+
+        [Fact]
+        public async Task Should_GetByIdSpaceship_WhenReturnsEqual()
+        {
+            Guid guid1 = Guid.Parse("204d3f40-0d9c-4adf-8bd2-602823455f47");
+            Guid guid = Guid.Parse("204d3f40-0d9c-4adf-8bd2-602823455f47");
+
+            // Call the GetAsync method with the guid parameter
+            var spaceship = await Svc<ISpaceshipServices>().GetAsync(guid);
+
+            // Check if the returned value is equal to the guid
+            Assert.Equal(guid, guid1); // Assuming that spaceship has an "Id" property            
+        }
+
+        [Fact]
+        public async Task Should_DeleteByIdSpaceship_WhenDeleteSpaceship()
+        {
+            //arrange 
+
+            SpaceshipDto spaceship = MockSpaceshipData();
+
+            //act
+
+            var addSpaceship = await Svc<ISpaceshipServices>().Create(spaceship);
+            var result = await Svc<ISpaceshipServices>().Delete((Guid)addSpaceship.Id);
+
+            //assert
+
+            Assert.Equal(result, addSpaceship);
+
+            
+        }
+
+        [Fact]
+        public async Task ShouldNot_DeleteByIdSpaceship_WhenDidNotDeleteSpaceship()
+        {
+            SpaceshipDto spaceship = MockSpaceshipData();
+
+            var addSpaceship = await Svc<ISpaceshipServices>().Create(spaceship);
+            var addSpaceship2 = await Svc<ISpaceshipServices>().Create(spaceship);
+
+            var result = await Svc<ISpaceshipServices>().Delete((Guid)addSpaceship2.Id);
+
+            Assert.NotEqual(result.Id, addSpaceship.Id);
+        }
+
+
+        private SpaceshipDto MockSpaceshipData()
+        {
+            SpaceshipDto spaceship = new()
+            {
+                Name = "asd",
+                Type = "asd",
+                Passengers = 123,
+                EnginePower = 123,
+                Crew = 123,
+                Company = "asd",
+                CargoWeight = 123,
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now,
+            };
+            return spaceship;
         }
     }
 }
